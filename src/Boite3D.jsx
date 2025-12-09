d7834861ddb3997e8c9a2d163668511002182a50
AubinManceau <aubinmanceau0@gmail.com>
2025-12-09 18:58:12 +0100

    add new 3d object and create music player

--- src/Boite3D.jsx
+++ src/Boite3D.jsx
@@ -1,30 +1,66 @@
+import { useEffect, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { Howl } from "howler";
 
-function Model({ url }) {
-    const { scene } = useGLTF(url);
-    scene.scale.set(10, 10, 10);
-    scene.traverse((child) => {
-        if (child.isMesh && child.name === "Cube032") {
-            console.log("Mesh trouvé :", child.name);
-            child.material.color.set("yellow"); // change la couleur
-        }
+function InteractiveModel({ modelPath, actions }) {
+  const gltf = useGLTF(modelPath);
+  const meshRefs = useRef({});
+
+  useEffect(() => {
+    gltf.scene.traverse((child) => {
+      if (child.isMesh && actions[child.name]) {
+        meshRefs.current[child.name] = child;
+      }
     });
+  }, [gltf, actions]);
 
-    return <primitive object={scene} />;
+  return (
+    <primitive
+      object={gltf.scene}
+      onClick={(e) => {
+        const mesh = e.object;
+        if (meshRefs.current[mesh.name] && actions[mesh.name].onClick) {
+          actions[mesh.name].onClick(mesh);
+        }
+      }}
+    />
+  );
 }
 
 export default function Boite3D() {
-    const handleMeshClick = (meshName) => {
-        console.log("Mesh cliqué :", meshName);
-    };
+  const music = useRef(
+    new Howl({
+      src: ["/audio/musique.mp3"],
+      loop: true,
+      volume: 0.5,
+    })
+  );
+
+  const meshActions = {
+    "Cube": {
+      onClick: (mesh) => {
+        console.log("Pause de la musique !", mesh.name);
+        music.current.pause();
+      },
+    },
+    "Cylindre": {
+      onClick: (mesh) => {
+        if (!music.current.playing()) {
+          console.log("Reprise de la musique !", mesh.name);
+          music.current.play();
+        }
+      },
+    },
+  };
+
   return (
-    <div className="w-full h-[600px]">
-      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+    <div className="w-full h-[300px]">
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 10, 5]} intensity={1} />
-        <Model url="/models/clavierrr.glb"  onMeshClick={handleMeshClick}/>
-        <OrbitControls enablePan={false} enableZoom={false} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <InteractiveModel modelPath="models/test.glb" actions={meshActions} />
+        <OrbitControls />
       </Canvas>
     </div>
   );
